6bc56ea031a30d9ba3b446cc28b091c7fcb2ef2b
zhaofeng-shu33 <616545598@qq.com>
2019-08-14 11:01:33 +0800

    basic drawing tuning

--- game.R
+++ game.R
@@ -6,8 +6,8 @@ C = X * Y
 # dictionary object, key is ladder start
 ladders = c(38,14,31,42,44,67,84,91,100)
 names(ladders) = c(1,4,9,21,36,51,28,71,80)
-chuntes = c(16)
-names(chuntes) = c(6)
+chuntes = c(6,11,26,19,60,53,24,73,75,78)
+names(chuntes) = c(16,49,47,62,64,56,87,92,95,98)
 board = list(ladders=ladders, chuntes=chuntes)
 
 get_coordinate = function(num){
@@ -25,5 +25,24 @@ show_board = function(board){
     plot.new()
     plot.window(xlim=c(0,X), ylim=c(0,Y))
     box()
+    par(xaxp=c(0,10,10))
+    par(yaxp=c(0,10,10))
+    axis(1)
+    axis(2,panel.first=grid())
+    x0_list = c()
+    y0_list = c()
+    x1_list = c()
+    y1_list = c()
+    for(i in names(board$ladders)){
+        xy0 = as.numeric(i)
+        xy1 = ladders[[i]]
+        xy0 = get_coordinate(xy0)
+        xy1 = get_coordinate(xy1)
+        x0_list = c(x0_list, xy0[[1]])
+        y0_list = c(y0_list, xy0[[2]])
+        x1_list = c(x1_list, xy1[[1]])
+        y1_list = c(y1_list, xy1[[2]])        
+    }
+    arrows(x0_list, y0_list, x1_list, y1_list, col='#FFAC33')
     # draw arrow one by one
 }
